56787adebe8b49628eaf56ad3d50b0b9f4a9810d
Alessandro Toia <gort818@gmail.com>
2019-09-29 22:17:56 -0700

    Set up command parser and list args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate clap;
+use clap::{App, AppSettings, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("savep")
+        .version(env!("CARGO_PKG_VERSION"))
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .author("Alessandro Toia <gort818@gmail.com>")
+        .about("Utlity to save and restore unix permissions")
+        .args_from_usage(
+            "-s, --save 'Save all current unix permissions,
+        -r, --restore 'Restore unix permissions'",
+        )
+        .get_matches();
 }
