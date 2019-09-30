1cedeac2954b75d64382218569943df6184bfbf5
Alessandro Toia <gort818@gmail.com>
2019-09-30 16:54:51 -0700

    set up commandline args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate clap;
-use clap::{App, AppSettings, Arg, SubCommand};
+extern crate xdg;
+use clap::{App, AppSettings, Arg};
 
 fn main() {
     let matches = App::new("savep")
@@ -7,9 +8,24 @@ fn main() {
         .setting(AppSettings::ArgRequiredElseHelp)
         .author("Alessandro Toia <gort818@gmail.com>")
         .about("Utlity to save and restore unix permissions")
-        .args_from_usage(
-            "-s, --save 'Save all current unix permissions,
-        -r, --restore 'Restore unix permissions'",
+        .arg(
+            Arg::with_name("save")
+                .short("s")
+                .long("save")
+                .help("Save unix permissions")
+                .conflicts_with("restore"),
+        )
+        .arg(
+            Arg::with_name("restore")
+                .short("r")
+                .long("restore")
+                .help("Restore unix permissions")
+                .conflicts_with("save"),
         )
         .get_matches();
+    if matches.is_present("save") {
+        println!("save");
+    } else if matches.is_present("restore") {
+        println!("restore");
+    }
 }
