cfd081bce3d2995cf689bf93bdee071ef2be5a3c
Alessandro Toia <gort818@gmail.com>
2019-10-05 11:56:25 -0700

    Change permissions to bash file and restore

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use clap::{App, AppSettings, Arg};
 use std::env;
 use std::fs::File;
 use std::io::Write;
-use std::path::Path;
+use std::process;
 #[macro_use]
 extern crate run_script;
 
@@ -32,16 +32,46 @@ fn main() {
         )
         .get_matches();
     if matches.is_present("save") {
-        println!("save");
         //let xdg_dirs = xdg::BaseDirectories::with_prefix("savep").unwrap();
         let config_path = xdg_dirs
             .place_config_file("restore.sh")
             .expect("cannot create configuration directory");
         println!("{:?}", xdg_dirs.get_config_home());
         let mut config_file = File::create(config_path).unwrap();
+        writeln!(config_file, "#!/bin/bash").expect("could not write to file");
         let (code, output, error) = run_script!(
             r#"
-        find ./ -depth  -printf 'chmod  %m %p \n'
+        find ~+ ~/ -depth  -printf 'chmod  %m %p \n'
+        exit 0
+        "#
+        )
+        .unwrap();
+
+        println!("Output: \n{}", output);
+        println!("Error: \n{}", error);
+        println!("Exit Code: \n{}", code);
+        config_file
+            .write_all(output.as_bytes())
+            .expect("unable to write");
+        println!("{:?}", xdg_dirs.get_config_home());
+    } else if matches.is_present("restore") {
+        let dir = env::set_current_dir(&config_dir);
+        match dir {
+            Ok(dir) => println!(
+                "Successfully changed working directory to {} {:?}",
+                config_dir.display(),
+                dir
+            ),
+            Err(error) => {
+                println!("Please run the save option first!\nError: {}", error);
+                process::exit(0);
+            }
+        }
+        println!("Restoring permissions please wait...");
+        let (code, output, error) = run_script!(
+            r#"
+         chmod +x ./restore.sh
+        ./restore.sh
         exit 0
         "#
         )
@@ -50,22 +80,5 @@ fn main() {
         println!("Exit Code: {}", code);
         println!("Output: {}", output);
         println!("Error: {}", error);
-        //println!("path{:#?}", config_file);
-        config_file
-            .write_all(output.as_bytes())
-            .expect("unable to write");
-        println!("{:?}", xdg_dirs.get_config_home());
-    } else if matches.is_present("restore") {
-        println!("restore");
-        let dir = env::set_current_dir(&config_dir);
-        match dir {
-            Ok(content) => println!(
-                "Successfully changed working directory to {}",
-                config_dir.display()
-            ),
-            Err(error) => {
-                println!("Please run the save option first!\nError: {}", error);
-            }
-        }
     }
 }
