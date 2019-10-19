dcccf79f7aa1689d5841b8bb60bba1499cf973f6
Alessandro Toia <gort818@gmail.com>
2019-10-18 18:39:17 -0700

    allow user to set path to save, and check if it exists

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #[macro_use]
 extern crate clap;
-use clap::{App, AppSettings, Arg};
+use clap::App;
 extern crate xdg;
 use std::env;
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::Write;
 use std::os::unix::fs::PermissionsExt;
 use std::process;
@@ -16,48 +16,25 @@ fn main() {
     let config_dir = xdg_dirs.get_config_home();
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
-        // .version(env!("CARGO_PKG_VERSION"))
-        // .setting(AppSettings::ArgRequiredElseHelp)
-        // .author("Alessandro Toia <gort818@gmail.com>")
-        // .about("Utlity to save and restore unix permissions")
-        // .arg(
-        //     Arg::with_name("save") // add option for location and if not provided use / "root"
-        //         .short("s")
-        //         .long("save")
-        //         .help("Save unix permissions")
-        //         .conflicts_with("restore"),
-        // )
-        // .arg(
-        //     Arg::with_name("restore")
-        //         .short("r")
-        //         .long("restore")
-        //         .help("Restore unix permissions")
-        //         .conflicts_with("save"),
-        // )
-        //.get_matches();
+    let dir_path = matches.value_of("PATH").unwrap();
+    let dir_arg = fs::read_dir(dir_path);
+    match dir_arg {
+        Ok(_dir_arg) => println!("path found {}\n", dir_path),
+        Err(e) => {
+            println!("That path does not exist: \n{}", e);
+            process::exit(0);
+        }
+    }
+
     if matches.is_present("save") {
         //let xdg_dirs = xdg::BaseDirectories::with_prefix("savep").unwrap();
         let config_path = xdg_dirs
             .place_config_file("restore.sh")
             .expect("cannot create configuration directory");
-        println!("{:?}", xdg_dirs.get_config_home());
+        //println!("{:?}", xdg_dirs.get_config_home());
         let mut config_file = File::create(config_path).unwrap();
         writeln!(config_file, "#!/bin/bash").expect("could not write to file");
-        // let (code, output, error) = run_script!(
-        //     r#"
-        // find ~+ ~/ -depth  -printf 'chmod  %m %p \n'
-        // exit 0
-        // "#
-        // )
-        // .unwrap();
-
-        // println!("Output: \n{}", output);
-        // println!("Error: \n{}", error);
-        // println!("Exit Code: \n{}", code);
-        for entry in WalkDir::new("/home/alessandro/test/")
-            .into_iter()
-            .filter_map(|e| e.ok())
-        {
+        for entry in WalkDir::new(dir_path).into_iter().filter_map(|e| e.ok()) {
             let entry = entry;
             let permissions = entry.metadata().unwrap().permissions().mode();
             let perm_oct = format!("{:o}", permissions);
