2ee21e18e69be12cde77f8236914b0ef61f9e13e
Alessandro Toia <gort818@gmail.com>
2019-10-20 21:48:14 -0700

    set default path

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate clap;
 use clap::App;
+extern crate dirs;
 extern crate xdg;
 mod save;
 use save::save;
@@ -15,7 +16,9 @@ fn main() {
     let config_dir = xdg_dirs.get_config_home();
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
-    let dir_path = matches.value_of("path").unwrap();
+    let home_dir = dirs::home_dir().unwrap();
+    let home = home_dir.to_string_lossy();
+    let dir_path = matches.value_of("path").unwrap_or(&home);
     if matches.is_present("save") {
         save(&xdg_dirs, &dir_path);
     } else if matches.is_present("restore") {
